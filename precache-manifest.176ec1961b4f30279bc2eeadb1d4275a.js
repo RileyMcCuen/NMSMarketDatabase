0e7ad65bb5f4d6d4619a721e2eab0cf1d28e2d50
Riley McCuen <riley.mccuen@wustl.edu>
2020-10-12 23:13:48 -0500

    Updates

---
+++ precache-manifest.176ec1961b4f30279bc2eeadb1d4275a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "256cc6d2b89020840e94d6f1615c5d5c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e389bdfea24146a06a38",
+    "url": "/static/css/main.3532f3fb.chunk.css"
+  },
+  {
+    "revision": "cca923bf838e9b1ffc5c",
+    "url": "/static/js/2.67c7b3fc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67c7b3fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e389bdfea24146a06a38",
+    "url": "/static/js/main.4b9e29b8.chunk.js"
+  },
+  {
+    "revision": "880ead2686ec4d3085c1",
+    "url": "/static/js/runtime-main.b40e28e2.js"
+  },
+  {
+    "revision": "3c152434134c9eceb0cb9e1fff50812d",
+    "url": "/static/media/Geo-Italic.3c152434.ttf"
+  },
+  {
+    "revision": "a850b28e70e30919cca63ff9fbe917b2",
+    "url": "/static/media/Geo-Regular.a850b28e.ttf"
+  }
+]);
